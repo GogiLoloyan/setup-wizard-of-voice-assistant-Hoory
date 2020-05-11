a6a5280e48220eb69b2c1a0ece195af29f10e08e
Gogi Loloyan <39570520+GogiLoloyan@users.noreply.github.com>
2020-05-11 12:41:10 +0400

    Update Icons.js

--- src/components/registrationSteps/step2/components/Icons.js
+++ src/components/registrationSteps/step2/components/Icons.js
@@ -4,7 +4,7 @@ function Icons({ getIcon, setGender }) {
   return (
     <div className="styles-configuration__icons">
       <div onClick={() => setGender("female")}>
-        {getIcon("female")}
+      <svg>{getIcon("female")}</svg>
       </div>
       <div onClick={() => setGender("male")}>
         {getIcon("male")}
